5268e654484d281fe4ad375436d545510da39d92
Jenry Bargola <jenrybargola7@gmail.com>
2023-09-12 05:43:40 +0800

    Update Template

--- resources/adminapp/js/cruds/IPCR/admin.vue
+++ resources/adminapp/js/cruds/IPCR/admin.vue
@@ -8,37 +8,7 @@
 							<div class="col-md-6">
 								<h4 class="card-title">IPCR</h4>
 							</div>
-						</div>
-					</div>
-
-					<div class="card-body">
-						<ul class="nav nav-pills nav-pills-primary" role="tablist">
-							<li class="nav-item">
-								<a
-									class="nav-link active"
-									data-toggle="tab"
-									href="#link1"
-									role="tablist"
-									aria-expanded="true"
-								>
-									Upload Files
-								</a>
-							</li>
-							<li class="nav-item">
-								<a
-									class="nav-link"
-									data-toggle="tab"
-									href="#link2"
-									role="tablist"
-									aria-expanded="false"
-								>
-									Manual IPCR
-								</a>
-							</li>
-						</ul>
-						<div class="tab-content tab-space">
-							<div class="tab-pane active" id="link1" aria-expanded="true">
-								<div class="col-md-2 mt-4">
+							<div class="col-md-2 mt-4">
 									<h4 style="color: black">Upload Template:</h4>
 								</div>
 								<div class="col-md-2 mt-4">
@@ -47,20 +17,10 @@
 								<div class="col-md-2 mt-4">
 									<button class="btn btn-sm btn-primary" @click="uploadFiles">Submit</button>
 								</div>
-							</div>
-							<div class="tab-pane" id="link2" aria-expanded="false">
-								Efficiently unleash cross-media information without cross-media value. Quickly
-								maximize timely deliverables for real-time schemas.
-								<br /><br />Dramatically maintain clicks-and-mortar solutions without functional
-								solutions.
-							</div>
-							<div class="tab-pane" id="link3" aria-expanded="false">
-								Completely synergize resource taxing relationships via premier niche markets.
-								Professionally cultivate one-to-one customer service with robust ideas.
-								<br /><br />Dynamically innovate resource-leveling customer service for state of the
-								art customer service.
-							</div>
 						</div>
+					</div>
+
+					<div class="card-body">
 						<div class="card">
 							<div class="card-header card-header-primary">
 								<h4 class="card-title">IPCR Templates</h4>
