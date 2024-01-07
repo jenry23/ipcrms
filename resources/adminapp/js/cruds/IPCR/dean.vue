101f8ad0380ac7a24e0310ba2356207362d8400d
Jenry Bargola <jenrybargola7@gmail.com>
2024-01-08 00:40:03 +0800

    Fixed Initial Test

--- resources/adminapp/js/cruds/IPCR/dean.vue
+++ resources/adminapp/js/cruds/IPCR/dean.vue
@@ -264,46 +264,51 @@ export default {
 	computed: {
 		numericalRating () {
 			let result = 0;
+			let total_ipcr_performance1 = 0;
+
 			_.each(this.templates.ipcr_function, (value, key) => {
 				let sum1 = 0;
+				let total_ipcr_performance2 = 0;
+
 				_.each(value.ipcr_subfunctions, (value2, key2) => {
 					let sum = 0;
+					let total_ipcr_performance3 = 0;
+
 					_.each(value2.ipcr_performance, (value3, key4) => {
-						if (value3.quality) {
-							sum += parseInt(value3.quality);
+						if (value3.asc) {
+							sum += parseInt(value3.asc);
 						}
+						total_ipcr_performance3 + 1;
 					})
+					total_ipcr_performance2 += total_ipcr_performance3
 					sum1 += sum;
 				})
+				total_ipcr_performance1 += total_ipcr_performance2;
 				result += sum1;
 			});
 
-			return Math.min(Math.max(result, 1), 5);
+			return Math.round(result / total_ipcr_performance1 * 100) / 100;
 		},
 
 		adjectivalRating () {
 			let value = null;
 
-			if (this.numericalRating === 5) {
-				value = 'Outstanding';
-			} else if(this.numericalRating === 4) {
+			if(this.numericalRating >= 4 && this.numericalRating < 5) {
 				value = 'Very Satisfactory';
-			} else if(this.numericalRating === 3) {
+			} else if(this.numericalRating >= 3 && this.numericalRating < 4) {
 				value = 'Satisfactory';
-			} else if(this.numericalRating === 2) {
+			} else if(this.numericalRating >= 2 && this.numericalRating < 3) {
 				value = 'Unsatisfactory';
-			} else if(this.numericalRating === 1) {
+			} else if(this.numericalRating >= 1 && this.numericalRating < 2) {
 				value = 'Poor';
+			} else {
+				value = 'Outstanding';
 			}
 
 			return value;
 		},
 
-		dateToday () {
-			let date = new Date();
-			this.templates.date_today = `${date.getMonth()}, ${date.getFullYear()}`;
-
-			return this.templates.date_today;
+ {
 		},
 	},
 
@@ -335,123 +340,7 @@ export default {
 			return sum;
 		},
 
-		setTarget (value) {
-
-		},
-
-		computeAverage (value, index2, index1, index) {
-			let sum = 0;
-
-			this.number1 = Math.min(5, Math.max(1, parseInt(value.quantity)));
-			this.number2 = Math.min(5, Math.max(1, parseInt(value.quality)));
-			this.number3 = Math.min(5, Math.max(1, parseInt(value.tar)));
-
-			// Calculate the average of the three numbers
-			sum = (this.number1 + this.number2 + this.number3) / 3;
-
-			this.templates.ipcr_function[index].ipcr_subfunctions[index1].ipcr_performance[index2].asc = sum
-		},
-
-		computedTarget (value, index2, index1, index) {
-			const startDate = new Date(value.date_completed);
-			const endDate = new Date(value.date_of_submission);
-
-			const timeDifference = endDate - startDate;
-			let rating = 0;
-			// Calculate the number of days late or early (assuming 24 hours per day)
-			const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
-
-			// Define your rating scale based on your criteria
-			if (daysDifference >= -3 && daysDifference <= 3) {
-				rating = 3; // On time
-			} else if (daysDifference < -3) {
-				rating = 1; // Extremely late
-			} else if (daysDifference >= 7 && daysDifference <= 8) {
-				rating = 4; // Extremely late
-			} else {
-				rating = 5; // Completed well ahead of schedule
-			}
-
-			this.templates.ipcr_function[index].ipcr_subfunctions[index1].ipcr_performance[index2].tar = rating;
-		},
-
-		editFiles (data) {
-			this.templates = JSON.parse(data.data);
-			this.templates.id = data.id;
-
-			this.signatures = [
-				{
-					'title': 'Discussed with:',
-					'name': data.faculty_id ? data.faculty.name : null,
-					'signature': data.faculty_signature ?? null,
-				}, {
-					'title': 'Assessed by:',
-					'name': data.dean_id ? data.dean.name : null,
-					'signature':  this.numericalRating > 0 ? data.dean_signature : null
-				},
-				{
-					'title': 'Checked by:',
-					'name': data.hrmo_id ? data.hrmo.name : null,
-					'signature': this.numericalRating > 0 ? data.hrmo_signature : null
-				},
-				{
-					'title': 'Final Rating:',
-					'name': data.campus_director_id ? data.campus_director.name : null,
-					'signature': this.numericalRating > 0 ? data.campus_director_signature : null
-				}]
-			console.log(this.signatures);
-		},
-
-		submitForm () {
-			axios.post(`ipcr-faculty-assesstment`, this.templates).then(response => {
-				this.$toast.success("IPCR Evaluate successfully saved!");
-				window.location.reload();
-			}).catch(error => {
-				let message = error.response.data.message || error.message
-				this.$toast.error(message);
-			})
-		},
-
-		downloadFiles (data) {
-			this.json = JSON.parse(data);
-			this.$refs.html2Pdf.generatePdf()
-		},
-
-		openFileInput () {
-			// Trigger a click event on the hidden file input when the button is clicked
-			this.$refs.fileInput.click();
-		},
-
-		handleFileUpload (event) {
-			let form = new FormData();
-			let selectedFile = event.target.files[0];
-
-			let data = {
-				'assessment_id': this.templates.id,
-				'files': selectedFile,
-				'is_dean': 1
-			}
-
-			_.each(data, (value, key) => {
-				form.append(key, value);
-			})
-
-			let config = {
-				header: {
-					'Content-Type': 'multipart/form-data',
-				}
-			}
-
-			axios.post(`ipcr-faculty-assesstment/upload-signature`, form, config).then(response => {
-				this.$toast.success("Signature Sucessfully Saved!");
-				this.fetchDeanIPCR();
-				window.location.reload();
-			}).catch(error => {
-				let message = error.response.data.message || error.message
-				this.$toast.error(message);
-			})
-			// You can now use the selectedFile object as needed
-		},
+,
 
 	}
 }
